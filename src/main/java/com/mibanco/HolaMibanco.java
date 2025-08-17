2c2e9327d024145172be6c7e47b5a8fa506386a0
StefanoCva <stefanocavalier07@gmail.com>
2025-08-17 16:34:42 -0500

    proyecto java

--- src/main/java/com/mibanco/HolaMibanco.java
+++ src/main/java/com/mibanco/HolaMibanco.java
@@ -1,17 +1,17 @@
 package com.mibanco;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
 @ApplicationPath("/api")
-@Path("/hello")
+@Path("/hola")
 public class HolaMibanco extends Application {
 
     @GET
-    public Response saludo() {
-        return Response.ok("Hola Mibanco").build();
+    public Response hola() {
+        return Response.ok("¡Hola Mibanco!").build();
     }
 }
